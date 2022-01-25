5f5ad67546a6348e7b4f5b107e1c3d223138e71c
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-01-25 14:58:33 +0530

    Superheroes_module

---
+++ Superheroes/villains module/index.js
@@ -0,0 +1,7 @@
+const supervillains=require('supervillains');
+const superheroes= require('superheroes');
+
+const mySuperheroes= superheroes.random();
+const mysupervillainsName= supervillains.random();
+console.log(mySuperheroes);
+console.log(mysupervillainsName);
